3763b9d537c298373d1b2d0eddb4b1192f4f9d94
olkku45 <olli.tervonen05@gmail.com>
2025-02-03 20:51:49 +0200

    animate tiles, some bugs

--- 2048.py
+++ 2048.py
@@ -26,7 +26,6 @@ tile_height = window_height // rows
 tile_width = window_width // cols
 
 outline_thickness = 6
-move_vel = 80
 
 font = pygame.font.SysFont("Raleway.ttf", 50)
 font_color_1 = (77, 77, 77)
@@ -50,43 +49,64 @@ background_color = (254, 255, 242)
 
 screen = pygame.display.set_mode((window_width, window_height))
 
+speed = 1  # pixel / ms
+
 
 class Tile:
 
 
     def __init__(self, value, row, col):
-        self.value = value 
+        self.value = value
+        self.next_value = value 
         self.row = row
         self.col = col
+        self.x = col * tile_width
+        self.y = row * tile_height
+    
+
+    def animate(self, dt):
+        go_x = self.col * tile_width
+        go_y = self.row * tile_height
+        if self.x == go_x and self.y == go_y:
+            self.value = self.next_value
+            return False # animation done
+        
+        entire_trip = math.sqrt((self.x - go_x) ** 2 + (self.y - go_y) ** 2)
+        
+        step = min(entire_trip, speed * dt)
+
+        if self.x < go_x:
+            dx = step
+        elif self.x > go_x:
+            dx = -step
+        else:
+            dx = 0
+        
+        if self.y < go_y:
+            dy = step
+        elif self.y > go_y:
+            dy = -step
+        else: 
+            dy = 0
+
+        self.x += dx
+        self.y += dy
+        return True
 
 
     def draw(self, screen):
         color = tile_colors[self.value]
-        pygame.draw.rect(screen, color, (self.col * tile_width, self.row * tile_height, tile_width, tile_height))
+        pygame.draw.rect(screen, color, (self.x, self.y, tile_width, tile_height))
 
         if self.value >= 8:
             text = font.render(str(self.value), 1, font_color_2)
         else:
             text = font.render(str(self.value), 1, font_color_1)
 
-        text_x = round(self.col * tile_width + (tile_width / 2 - text.get_width() / 2))
-        text_y = round(self.row * tile_height + (tile_height / 2 - text.get_height() / 2))
+        text_x = round(self.x + (tile_width / 2 - text.get_width() / 2))
+        text_y = round(self.y + (tile_height / 2 - text.get_height() / 2))
         
         screen.blit(text, (text_x, text_y))
-
-
-    ''' def set_pos(self, ceil=False):
-        if ceil:
-            self.row = math.ceil(self.y / tile_height)
-            self.col = math.ceil(self.x / tile_width)
-        else:
-            self.row = math.floor(self.y / tile_height)
-            self.col = math.floor(self.x / tile_width)'''
-
-
-    '''def move(self, delta):
-        self.x += delta[0]
-        self.y += delta[1]'''
 
 
 def draw_grid(screen):
@@ -101,7 +121,10 @@ def draw_grid(screen):
         pygame.draw.line(screen, outline_color, (x, 0), (x, window_height), outline_thickness)
 
 
-def draw(screen, tiles):
+def draw(screen, tiles, removed_tiles):
+    for tile in removed_tiles:
+        tile.draw(screen)
+
     screen.fill(background_color)
 
     for tile in tiles.values():
@@ -136,6 +159,8 @@ def generate_tiles():
 
 def move_tiles(tiles, direction): 
         
+    removed_tiles = []
+    
     if direction == "left":
         dx = -1
         dy = 0
@@ -159,7 +184,7 @@ def move_tiles(tiles, direction):
     merged = []
 
     for tile in sorted_tiles.copy():
-        while tile.col + dx in range(4) and tile.row + dy in range(4)  and f"{tile.row + dy}{tile.col + dx}" not in tiles:
+        while tile.col + dx in range(4) and tile.row + dy in range(4) and f"{tile.row + dy}{tile.col + dx}" not in tiles:
             tile.col += dx
             tile.row += dy
             update_tiles(tiles, sorted_tiles)
@@ -171,10 +196,12 @@ def move_tiles(tiles, direction):
         else:
             merge_value = tile.row
 
-        if next_tile_key in tiles and tiles[next_tile_key].value == tile.value and merge_value not in merged:
-            print("merge")
-            sorted_tiles.remove(tile)
-            tiles[next_tile_key].value *= 2
+        if next_tile_key in tiles and tiles[next_tile_key].next_value == tile.next_value and merge_value not in merged:
+            sorted_tiles.remove(tiles[next_tile_key])
+            removed_tiles.append(tiles[next_tile_key])
+            tile.next_value *= 2
+            tile.row += dy
+            tile.col += dx
             update_tiles(tiles, sorted_tiles)
             if dx == 0:
                 #vertical
@@ -182,13 +209,30 @@ def move_tiles(tiles, direction):
             else:
                 merged.append(tile.row)
     
-    #TODO: tämä lopettaa pelin aina kun ruudut ovat täynnä, mutta voi vielä pystyä siirtämään joskus
+    #TODO: tämä lopettaa pelin aina kun ruudut ovat täynnä, mutta voi kuitenkin pystyä vielä siirtämään joskus
     if len(tiles) == 16:
-        return "lost"
+        return "lost", removed_tiles
+
     else:
-        row, col = get_random_pos(tiles)
-        tiles[f"{row}{col}"] = Tile(random.choice([2, 4]), row, col)
-        return "continue"
+        return "continue", removed_tiles
+
+
+def animate_tiles(tiles, dt, clock, removed_tiles):
+    while True:
+        need_more = False
+        for tile in tiles.values():
+            if tile.animate(dt):
+                need_more = True
+        if not need_more:
+            break
+    
+        draw(screen, tiles, removed_tiles)
+
+        pygame.display.flip()
+        dt = clock.tick(60)
+
+    row, col = get_random_pos(tiles)
+    tiles[f"{row}{col}"] = Tile(random.choice([2, 4]), row, col)
     
 
 def update_tiles(tiles, new_tiles):
@@ -196,6 +240,7 @@ def update_tiles(tiles, new_tiles):
     for tile in new_tiles:
         tiles[f"{tile.row}{tile.col}"] = tile
 
+#TODO: näyttö pelin lopun jälkeen, jossa näkyy score ja "play again" nappi, kuten oikeassa 2048:ssa
 
 def main(screen):
     clock = pygame.time.Clock()
@@ -210,18 +255,22 @@ def main(screen):
 
             if event.type == pygame.KEYDOWN: 
                 if event.key == pygame.K_a:
-                    move_tiles(tiles, "left")
+                    _, removed_tiles = move_tiles(tiles, "left")
+                    animate_tiles(tiles, dt, clock, removed_tiles)
                 elif event.key == pygame.K_d:
-                    move_tiles(tiles, "right")
+                    _, removed_tiles = move_tiles(tiles, "right")
+                    animate_tiles(tiles, dt, clock, removed_tiles)
                 elif event.key == pygame.K_w:
-                    move_tiles(tiles, "up")
+                    _, removed_tiles = move_tiles(tiles, "up")
+                    animate_tiles(tiles, dt, clock, removed_tiles)
                 elif event.key == pygame.K_s:
-                    move_tiles(tiles, "down")
+                    _, removed_tiles = move_tiles(tiles, "down")
+                    animate_tiles(tiles, dt, clock, removed_tiles)
 
-        draw(screen, tiles)
+        draw(screen, tiles, [])
 
         pygame.display.flip()
-        clock.tick(60)
+        dt = clock.tick(60)
 
 
 main(screen)
